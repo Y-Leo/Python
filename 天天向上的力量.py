5da02cf075c85f3c4692b9ac40301df0b2e712c7
Y-Leo <964952155@qq.com>
2020-04-19 18:02:14 +0800

    天天向上的力量

---
+++ 天天向上的力量.py
@@ -0,0 +1,34 @@
+#天天向上的力量
+'''
+#DayDayUpQ1.py
+dayfactor = 0.005
+dayup = pow(1 + dayfactor,365)
+daydown = pow(1 - dayfactor,365)
+print("向上：{:.2f}，向下:{:.2f}".format(dayup,daydown))
+'''
+
+'''
+#DayDayUpQ2.py
+dayup = 1.0
+dayfactor = 0.01
+for i in range(365):
+    if i % 7 in [6,0]:
+        dayup = dayup * (1 - dayfactor)
+    else:
+        dayup = dayup * (1 + dayfactor)
+print("工作日的力量:{:.2f}".format(dayup))
+'''
+
+#DayDayUpQ3.py
+def dayUP(df):
+    dayup = 1
+    for i in range(365):
+        if i % 7 in [6,0]:
+            dayup = dayup * (1 - 0.001)
+        else:
+            dayup = dayup * (1 + df)
+    return dayup
+dayfactor = 0.01
+while dayUP(dayfactor) < 37.78:
+    dayfactor += 0.001
+print("工作日的努力参数是:{:.3f}".format(dayfactor))
